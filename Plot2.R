dfe243cc54214fed8ff1cb53b92347ed06ae8a02
Prasanna Nadimpalli <iamthe007@gmail.com>
2016-04-09 21:02:52 -0600

    updated with comments

--- Plot2.R
+++ Plot2.R
@@ -1,13 +1,17 @@
 library(lubridate)
 
+## get the input data
 powerData <- read.csv("household_power_consumption.txt", sep =";", na.strings ="?")
 
 powerDataFiltered <- subset(powerData, Date == "1/2/2007" | Date =="2/2/2007")
 
 png(file="plot2.png",width=480,height=480,units='px')
 
+##Get the data and time column into POSIX format
+
 powerDataFiltered.2 <- transform(powerDataFiltered, Datetime = as.POSIXlt(dmy(powerDataFiltered$Date)+hms(powerDataFiltered$Time)))
 
+## Plot the graph
 plot(powerDataFiltered.2$Datetime,as.numeric(as.character(powerDataFiltered.2$Global_active_power)),ylab='Global Active Power (kW)', xlab='', type='l')
 
 dev.off()
