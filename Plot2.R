682c4941b2a2897fc1bf6893dca4fe9842386163
Prasanna Nadimpalli <iamthe007@gmail.com>
2016-04-09 20:35:04 -0600

    Create Plot2.R

---
+++ Plot2.R
@@ -0,0 +1,13 @@
+library(lubridate)
+
+powerData <- read.csv("household_power_consumption.txt", sep =";", na.strings ="?")
+
+powerDataFiltered <- subset(powerData, Date == "1/2/2007" | Date =="2/2/2007")
+
+png(file="plot2.png",width=480,height=480,units='px')
+
+powerDataFiltered.2 <- transform(powerDataFiltered, Datetime = as.POSIXlt(dmy(powerDataFiltered$Date)+hms(powerDataFiltered$Time)))
+
+plot(powerDataFiltered.2$Datetime,as.numeric(as.character(powerDataFiltered.2$Global_active_power)),ylab='Global Active Power (kW)', xlab='', type='l')
+
+dev.off()
